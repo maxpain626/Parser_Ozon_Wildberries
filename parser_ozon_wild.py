a2f0015870eb469c9d53d3c81aab1ccb0af1c759
maxpain626 <46bublik@gmail.com>
2024-02-13 07:35:34 +0300

    makefile editing

--- parser_ozon_wild.py
+++ parser_ozon_wild.py
@@ -2,5 +2,11 @@ from urllib.request import Request
 
 
 req = Request('https://www.ozon.ru/product/tecno-smartfon-pova-neo-3-8-128-gb-chernyy-1090852191/')
+req.add_header('Accept-Encoding', 'gzip, deflate, br')
+#req.add_header()
+#req.add_header()
+#req.add_header()
+#req.add_header()
+content = urllib(req).read()
 
-
+print()
